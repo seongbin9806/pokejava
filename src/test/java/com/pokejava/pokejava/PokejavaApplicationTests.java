63fc155098e633ac2131ae3b89d10282a27a7c11
seongbin9806 <seongbin9806@naver.com>
2024-10-16 12:14:11 +0900

    spring init

---
+++ src/test/java/com/pokejava/pokejava/PokejavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokejava.pokejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokejavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
