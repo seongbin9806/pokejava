506b3a0f9cd39064353b8afd18e83167d7105e19
seongbin9806 <seongbin9806@naver.com>
2024-10-21 06:00:27 +0900

    Random PokeAPI

---
+++ pokejava/src/main/java/com/pokejava/pokejava/PokejavaApplication.java
@@ -0,0 +1,13 @@
+package com.pokejava.pokejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokejavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokejavaApplication.class, args);
+	}
+
+}
