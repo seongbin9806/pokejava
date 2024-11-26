7c0f51a7d0c02d42ba454ff7d1f3e9eada076eba
seongbin9806 <seongbin9806@naver.com>
2024-11-27 00:08:17 +0900

    get Model View

--- src/main/java/com/pokejava/pokejava/repository/MemberRepository.java
+++ src/main/java/com/pokejava/pokejava/repository/MemberRepository.java
@@ -1,8 +1,9 @@
 package com.pokejava.pokejava.repository;
 
 import com.pokejava.pokejava.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface MemberRepository extends CrudRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
     Member findByMemberEmail(String memberEmail);  // 이메일로 회원 검색
 }
--- src/main/java/com/pokejava/pokejava/repository/MyPokeRepository.java
+++ src/main/java/com/pokejava/pokejava/repository/MyPokeRepository.java
@@ -1,8 +1,9 @@
 package com.pokejava.pokejava.repository;
 
 import com.pokejava.pokejava.entity.MyPokemon;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface MyPokeRepository extends CrudRepository<MyPokemon, Long> {
+public interface MyPokeRepository extends JpaRepository<MyPokemon, Long> {
 
 }
